99ddb8a2984da2c4ed8814536c9a111cac6f6a23
Tracey-Copeland <141526105+Tracey-Copeland@users.noreply.github.com>
2023-08-22 13:39:46 -0400

    Add files via upload

---
+++ Security.jsx
@@ -0,0 +1,64 @@
+import { useEffect } from "react";
+import React, {useState} from "react";
+import { Navigate } from "react-router-dom";
+import { Link } from 'react-router-dom';
+
+import "./Security.css"
+
+
+const Security = () =>{
+    const [isNotOfAge, setIsNotOfAge] = useState(true);
+
+    const [redirctTo, setRedirctTo] = useState(false);
+
+    useEffect(() => {
+        if(sessionStorage.getItem("notage21")){
+            setIsNotOfAge(sessionStorage.getItem("notage21"));
+            setRedirctTo(true);
+        }
+
+        if (!isNotOfAge){
+            setRedirctTo(true);
+        }
+        console.log(redirctTo);
+
+    },[isNotOfAge,redirctTo])
+
+    const handleModalClose = () =>{
+        sessionStorage.setItem("notage21", false);
+
+    }
+
+
+if(redirctTo){
+    return <Navigate to="/home" />
+} else 
+    return(
+    <>
+        <div className="overlay">
+            <div className="modalContainer">
+                <div className="modalContent">
+                    <img src="/images/Element_MD_logo.png" alt="Element MD logo"/>
+
+                    <h1>You need to be over the age of 21 to view this site</h1>
+
+                    <p>Are you 21 years of age or older?</p>
+                <div className="modalbuttons">
+                    <button> 
+                        <span className="bold">No</span>, I'm not 21</button>
+                        
+                    <Link to="/home">
+                    <button onClick= {handleModalClose}>
+                        <span className="bold">Yes</span>, I'm 21</button>
+                    </Link>
+                </div>
+                </div>
+            </div>
+        </div>
+        
+    </>
+    )
+
+}
+
+export default Security;
