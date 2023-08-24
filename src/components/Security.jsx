e6598301f43db239cc09bd1c6ee668fc6c4b1845
Tracey-Copeland <141526105+Tracey-Copeland@users.noreply.github.com>
2023-08-24 16:54:56 -0400

    Add files via upload

--- src/components/Security.jsx
+++ src/components/Security.jsx
@@ -11,6 +11,8 @@ const Security = () =>{
 
     const [redirctTo, setRedirctTo] = useState(false);
 
+    const [clickedNo, setClickedNo] = useState(false);
+
     useEffect(() => {
         if(sessionStorage.getItem("notage21")){
             setIsNotOfAge(sessionStorage.getItem("notage21"));
@@ -28,6 +30,11 @@ const Security = () =>{
         sessionStorage.setItem("notage21", false);
 
     }
+    
+    const handleClickedNo = () =>{
+        setClickedNo(!clickedNo);
+
+    }
 
 
 if(redirctTo){
@@ -43,8 +50,13 @@ if(redirctTo){
                     <h1>You need to be over the age of 21 to view this site</h1>
 
                     <p>Are you 21 years of age or older?</p>
+
+                    {clickedNo && (
+                        <div className="noPopup">You need to be 21 to enter this site</div>
+                    )}
+
                 <div className="modalbuttons">
-                    <button> 
+                    <button onClick={handleClickedNo}> 
                         <span className="bold">No</span>, I'm not 21</button>
                         
                     <Link to="/home">
@@ -52,6 +64,7 @@ if(redirctTo){
                         <span className="bold">Yes</span>, I'm 21</button>
                     </Link>
                 </div>
+                
                 </div>
             </div>
         </div>
